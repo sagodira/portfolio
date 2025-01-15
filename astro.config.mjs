765b0b14675fcee0b9f640438364c8731381c000
sagodira <mathys.sagodira@etu.unilim.fr>
2025-01-15 17:34:50 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sagodira.github.io/portfolio/',
-    base:'/portfolio/',
+    site: 'https://sagodira.github.io',
     integrations: [mdx(), sitemap(), react(), tailwind()],
 });
